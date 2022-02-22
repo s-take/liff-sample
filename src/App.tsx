43b69929b341233366401404cc6ee764074e3af1
s-take <s-take@nec.com>
2022-02-22 09:21:02 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import liff from '@line/liff'
+import './App.css'
 
 function App() {
+  let isLoggedIn = false
+  try {
+    isLoggedIn = liff.isLoggedIn()
+  } catch (e) {
+    console.log(e)
+  }
+  const sendMessage = () => {
+    liff
+      .sendMessages([
+        {
+          // メッセージを送信する
+          type: 'text',
+          text: "You've successfully sent a message! Hooray!",
+        },
+      ])
+      .then(function () {
+        window.alert('Message sent')
+      })
+      .catch(function (error) {
+        window.alert('Error sending message: ' + error)
+      })
+  }
+
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff
+      .getProfile() // ユーザ情報を取得する
+      .then((profile) => {
+        const userId: string = profile.userId
+        const displayName: string = profile.displayName
+        alert(`Name: ${displayName}, userId: ${userId}`)
+      })
+      .catch(function (error) {
+        window.alert('Error sending message: ' + error)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React LIFF Sample</p>
+        {isLoggedIn ? (
+          <>
+            <button className="button" onClick={sendMessage}>
+              send message
+            </button>
+            <button className="button" onClick={getUserInfo}>
+              show user info
+            </button>
+          </>
+        ) : (
+          <p>please login</p>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import liff from '@line/liff'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+liff
+  .init({
+    liffId: process.env.REACT_APP_LIFF_ID || '',
+    withLoginOnExternalBrowser: true,
+  })
+  .then(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    )
+  })
+  .catch((e) => {
+    alert(`LIFF error: ${e.message}`)
+  })
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
