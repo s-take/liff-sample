7d3b4243059a8c8cf549914c495cb55bfbe041d5
iwahiro <pig.figg.fong.5963@gmail.com>
2022-02-23 20:17:46 +0900

    update to use mui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,41 @@
 import React from 'react'
 import liff from '@line/liff'
-import './App.css'
+import { AppBar, Typography, Toolbar } from '@mui/material'
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import Main from './pages/main'
+
+const Container = styled('div')({
+  margin: 0,
+  padding: '1em',
+})
 
 function App() {
+  const theme = createTheme()
   let isLoggedIn = false
   try {
     isLoggedIn = liff.isLoggedIn()
   } catch (e) {
     console.log(e)
   }
-  const sendMessage = () => {
-    liff
-      .sendMessages([
-        {
-          // メッセージを送信する
-          type: 'text',
-          text: "You've successfully sent a message! Hooray!",
-        },
-      ])
-      .then(function () {
-        window.alert('Message sent')
-      })
-      .catch(function (error) {
-        window.alert('Error sending message: ' + error)
-      })
-  }
-
-  /* 追加: UserProfileをAlertで表示 */
-  const getUserInfo = () => {
-    liff
-      .getProfile() // ユーザ情報を取得する
-      .then((profile) => {
-        const userId: string = profile.userId
-        const displayName: string = profile.displayName
-        alert(`Name: ${displayName}, userId: ${userId}`)
-      })
-      .catch(function (error) {
-        window.alert('Error sending message: ' + error)
-      })
-  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>React LIFF Sample</p>
-        {isLoggedIn ? (
-          <>
-            <button className="button" onClick={sendMessage}>
-              send message
-            </button>
-            <button className="button" onClick={getUserInfo}>
-              show user info
-            </button>
-          </>
-        ) : (
-          <p>please login</p>
-        )}
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        sx={{
+          position: 'relative',
+          borderBottom: (t) => `1px solid ${t.palette.divider}`,
+        }}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            React LIFF Sample
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>{isLoggedIn ? <Main /> : <p>please login</p>}</Container>
+    </ThemeProvider>
   )
 }
 
